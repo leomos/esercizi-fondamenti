9760990ff0c8e88c3c826a0b97fdcac6146b96a6
Leonardo Mosciatti <leonardo@mweb.biz>
2016-02-06 12:19:57 +0100

    Normalized tabs and spaces for syllables.c

--- syllables.c
+++ syllables.c
@@ -1,20 +1,20 @@
 /*
 		text: |
-            Scrivere una funzione `sillaba` che, presa in
-            ingresso una stringa,
-            la divida in sillabe. La regola è: se la parola
-            è del tipo "cvcvcv...." (dove 'c' rappresenta una consonante e 'v' una vocale) , i gruppi 'cv' formano una sillaba.
-            Se la parola è del tipo 'vcvcvcv....' la prima vocale fa
-            sillaba a sé, e per le altre vale la regola di sopra
-        esempi:
-            - e-va-de-re
-            - a-si-no
-            - ca-sa
-            - di-va-ga-va-no
-        test: |
-            assert(strcmp("evadere","e-va-de-re")==0)
-            assert(strcmp("asino,"a-si-no")==0)
-            assert(strcmp("casa","ca-sa")==0)
+			Scrivere una funzione `sillaba` che, presa in
+			ingresso una stringa,
+			la divida in sillabe. La regola è: se la parola
+			è del tipo "cvcvcv...." (dove 'c' rappresenta una consonante e 'v' una vocale) , i gruppi 'cv' formano una sillaba.
+			Se la parola è del tipo 'vcvcvcv....' la prima vocale fa
+			sillaba a sé, e per le altre vale la regola di sopra
+		esempi:
+			- e-va-de-re
+			- a-si-no
+			- ca-sa
+			- di-va-ga-va-no
+		test: |
+			assert(strcmp("evadere","e-va-de-re")==0)
+			assert(strcmp("asino,"a-si-no")==0)
+			assert(strcmp("casa","ca-sa")==0)
 */
 
 
@@ -28,17 +28,17 @@ int is_vowel(char c) {
 }
 
 int is_consonant(char c){
-    return ((c > 'a') && (c <= 'z')) || ((c>'A') && (c <= 'Z')) && !is_vowel(c);
+	return ((c > 'a') && (c <= 'z')) || ((c>'A') && (c <= 'Z')) && !is_vowel(c);
 }
 
 char *extract_first_syllable_starting_from_vowel(char *chunk){
-    char *result = (char *)malloc(sizeof(char) * 2);
+	char *result = (char *)malloc(sizeof(char) * 2);
 	result[0] = *chunk;
 	result[1] = '\0';
 	return result;
 }
 char *extract_first_syllable_starting_from_consonant(char *chunk){
-    char *result = (char *)malloc(sizeof(char) * 2);
+	char *result = (char *)malloc(sizeof(char) * 2);
 	result[0] = chunk[0];
 	result[1] = chunk[1];
 	result[2] = '\0';
@@ -47,11 +47,11 @@ char *extract_first_syllable_starting_from_consonant(char *chunk){
 
 char *extract_first_syllable(char *chunk){
 	char *result = strdup(".");
-    if(is_vowel(*chunk)){
-        return extract_first_syllable_starting_from_vowel(chunk);
-    } else {
+	if(is_vowel(*chunk)){
+		return extract_first_syllable_starting_from_vowel(chunk);
+	} else {
 		return extract_first_syllable_starting_from_consonant(chunk);
-    }
+	}
 	return result;
 }
 
